a5703f5fe513a404be597c45c66c655471ec9f7b
Nick-Anderssohn <7774240+Nick-Anderssohn@users.noreply.github.com>
2026-04-21 07:47:11 +0000

    nice 0.1.6

--- Casks/nice.rb
+++ Casks/nice.rb
@@ -1,6 +1,6 @@
 cask "nice" do
-  version "0.1.5"
-  sha256 "446fa202bc65e8b1e0bcc185abba6983fa8ac34d0be3433fadda664e01b5021d"
+  version "0.1.6"
+  sha256 "c1cdef7ea587b856bb78578e2205965cad3f7fd4f0a3e71b3574a3363ad60c49"
 
   url "https://github.com/Nick-Anderssohn/nice/releases/download/v#{version}/Nice-#{version}.zip"
   name "Nice"
